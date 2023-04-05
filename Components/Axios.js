f3bab25b4cc317a45649ca7df9bb89e7ab434ea2
smartkeerthi <keerthianbu2000@gmail.com>
2023-04-05 14:34:13 +0530

    upgraded to sdk 48

--- Components/Axios.js
+++ Components/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://famousplacesapi.herokuapp.com/api/v1'
+    baseURL: 'https://famous-place-api.onrender.com/api/v1'
 })
 
 export default instance;
